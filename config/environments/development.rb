2c4bb92e8885d0f8b5cc6226674ec1ade682909e
Ubuntu <ubuntu@ip-172-31-18-168.us-east-2.compute.internal>
2022-01-31 18:29:01 +0000

    ass home controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  config.hosts << "ec2-18-224-1-168.us-east-2.compute.amazonaws.com"
 
   # Do not eager load code on boot.
   config.eager_load = false
