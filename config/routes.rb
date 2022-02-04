7fd18933b922beca2b4e4eb71657e3314c9bd991
njaffer <njaffer@umich.edu>
2022-02-04 16:56:39 -0500

    add archives routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   devise_for :views
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'home#index'
+
+resources :archives do
+    collection do
+      get :new
+      post :save, :import
+    end
+  end 
 end
