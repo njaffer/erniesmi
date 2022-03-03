8420d54c22f54a4c94cdb095b8682f296ae217aa
njaffer <njaffer@umich.edu>
2022-03-03 17:40:46 -0500

    fix advanced search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'home#index'
 
+get 'index' => "home#index"
+
 resources :archives do
     collection do
       get :index, :new, :advanced_search, :test, :search, :letter, :county, :city, :category, :edit
